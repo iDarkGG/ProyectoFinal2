9167ec6614ef9a7a7f2d5c3f018e7637de7046e1
Wilman <caracol293@gmail.com>
2023-05-31 08:54:12 -0600

    Update ProductContext.cs

--- Dependencias/Contexts/ProductContext.cs
+++ Dependencias/Contexts/ProductContext.cs
@@ -4,9 +4,11 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Dependencias.Models;
 
-public class ProductContext : DbContext
+public class MainContext : DbContext
 {
     public DbSet<Product> Products { get; set; }
+    public DbSet<User> Users { get; set; }
+    
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         optionsBuilder.UseSqlServer("Server=DESKTOP-26H39J7\\SQLSERVER2019;Database=Product;Trusted_Connection=True;TrustServerCertificate=True");
