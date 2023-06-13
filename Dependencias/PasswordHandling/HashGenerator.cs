0ae99d6f992173a6ff9271a8e75a89a8b15f16a4
iDarkGG <harryriva14@gmail.com>
2023-06-13 10:25:28 -0600

    fix 2.1

--- Dependencias/PasswordHandling/HashGenerator.cs
+++ Dependencias/PasswordHandling/HashGenerator.cs
@@ -13,9 +13,20 @@ namespace Dependencias.PasswordHandling
         private string stringtoHash;
         private byte[] tmpStore;
         private byte[] hash;
-
+        private string finalHash;
         
-        
+        public void EncryptPassword()
+        {
+                tmpStore = ASCIIEncoding.ASCII.GetBytes(stringtoHash);
+                hash = new MD5CryptoServiceProvider().ComputeHash(tmpStore);
+            int i;
+            StringBuilder sOutput = new StringBuilder(hash.Length);
+            for (i = 0; i < hash.Length; i++)
+            {
+                sOutput.Append(hash[i].ToString("X2"));
+            }
+             finalHash = sOutput.ToString();
+        }
 
     }
 }
