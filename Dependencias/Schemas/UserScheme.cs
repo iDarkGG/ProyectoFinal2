b17b22021dc9e4f21396559b962a5a2530c508bb
Akkarin <50252542+iDarkGG@users.noreply.github.com>
2023-06-20 12:14:51 -0600

    Schemas FIX

---
+++ Dependencias/Schemas/UserScheme.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Dependencias
+{
+    public class UserScheme
+    {
+        public string userName { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+    }
+}
